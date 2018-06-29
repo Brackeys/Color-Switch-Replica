29b4324a4dced69b79d0c2559aae481815c9e366
YagoCADC <yago.cadc@gmail.com>
2018-06-28 23:56:08 -0300

    Clean Code
    some code.

---
+++ Color Switch Replica/Assets/ColorsType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ColorSwitchReplica
+{
+    public enum ColorsType
+    {
+        Cyan = 0,
+        Yellow = 1,
+        Magenta = 2,
+        Pink = 3
+    }
+}
